b656af61a4ae1b5b4c3a59f4c2c8708f8524b21b
chee <chee@snaek.org>
2016-03-22 12:06:55 +0000

    run messages through mods

--- bot.py
+++ bot.py
@@ -1,6 +1,34 @@
-from irc import bot
+#!/usr/bin/env python
+import irc.bot
+import os
+import subprocess
 
-config_file = open('config', 'r')
-config = dict([line.strip('\n').split() for line in config_file.readlines()])
-config_file.close()
-print(config)
+def config(key):
+    return open('conf/' + key).readline().strip('\n')
+
+class Petal(irc.bot.SingleServerIRCBot):
+    def __init__(self, channel, name, server, port):
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, int(port))], name, name)
+        self.channel = channel
+
+    def on_welcome(self, c, e):
+        c.join(self.channel)
+
+    def on_privmsg(self, c, e):
+        self.message(e, e.arguments[0])
+
+    def on_pubmsg(self, c, e):
+        self.message(e, e.arguments[0])
+
+    def message(self, e, msg):
+        for mod in os.listdir('mods'):
+            result = subprocess.check_output(['mods/' + mod, msg])
+            self.connection.privmsg(config('channel'), result.decode().strip())
+
+
+def main():
+    bot = Petal(config('channel'), config('name'), config('server'), config('port'))
+    bot.start()
+
+if __name__ == "__main__":
+    main()
