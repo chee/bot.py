6be769c329ef0afc53faa3f6ffdd7df3d14ec91c
chee <chee@snaek.org>
2016-03-23 21:36:00 +0000

    remove superfluous semicolon

--- mods/echo.js
+++ mods/echo.js
@@ -3,5 +3,5 @@ var process = require('process')
 message = process.argv.splice(2).join(' ')
 
 if (message.indexOf('&echo') == 0) {
-    console.log(message.substring(5));
+    console.log(message.substring(5))
 }
